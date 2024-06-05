34a158b9ff57a1480b4e3bceed58edf566cbd865
elias provincia <provinciaelias@gmail.com>
2024-06-05 00:07:09 -0500

    feat: add ARG and ENV usuarios

--- msvc-usuarios/src/main/java/com/elias/springcloud/msvc/usuarios/cllients/CursoClientRest.java
+++ msvc-usuarios/src/main/java/com/elias/springcloud/msvc/usuarios/cllients/CursoClientRest.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "mcsv-cursos",url ="mcsv-cursos:8002" )
+@FeignClient(name = "mcsv-cursos",url ="${msv.cursos.url}" )
 public interface CursoClientRest {
     @DeleteMapping("/cursos/eliminar-curso-usuario/{id}")
     void eliminarCursoUsuarioPorId(@PathVariable Long id);
